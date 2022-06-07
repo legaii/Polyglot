516c130334c77f19f2314886c252aa8cbe10ef61
legaii <jgates.ardux@gmail.com>
2022-06-07 13:12:07 +0300

    fix Translate.py stdin bug

--- Translate.py
+++ Translate.py
@@ -1,13 +1,12 @@
 import sys
 import requests
-import fileinput
 
 def translate(api_key, lang, text):
   return requests.get(f"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={api_key}&lang={lang}&text={text}").json()
 
 def main():
   assert len(sys.argv) == 3
-  for line in fileinput.input():
+  for line in sys.stdin.readlines():
     for item_from in translate(sys.argv[1], sys.argv[2], line)["def"]:
       print("; ".join(item_to["text"] for item_to in item_from["tr"]) + f" ({item_from.get('pos', 'unknown')})", item_from["text"], 0, sep=" = ")
 
